e8060d710bf6f9bb3e8817461099b663d5ff70f4
Samaroo <nsa20@ostml0244845.bskyb.com>
2018-02-08 15:17:50 +0000

    add version check and default vsphere meta data

--- discovery/vsphere/vsphere.go
+++ discovery/vsphere/vsphere.go
@@ -1,34 +1,39 @@
 package vsphere
 
 import (
-	"fmt"
-	"net/url"
-	"time"
 	"context"
+	"fmt"
 	"net"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
-	"github.com/go-kit/kit/log/level"
 	"github.com/go-kit/kit/log"
-	"github.com/vmware/govmomi/vim25/debug"
-	"github.com/vmware/govmomi"
-	"github.com/vmware/vic/pkg/vsphere/tags"
-	"github.com/vmware/govmomi/view"
-	"github.com/vmware/govmomi/vim25/mo"
-	"github.com/prometheus/prometheus/util/strutil"
-	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/discovery/targetgroup"
+	"github.com/prometheus/prometheus/util/strutil"
 	yaml_util "github.com/prometheus/prometheus/util/yaml"
-
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/debug"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
+	"github.com/vmware/vic/pkg/vsphere/tags"
 )
 
 //TODO Remove hardcoded value
 const (
 	defaultAPITimeout = time.Minute * 5
-	vSphereLabel = model.MetaLabelPrefix + "vsphere_"
-	)
+	vSphereLabel      = model.MetaLabelPrefix + "vsphere_"
+
+	vSphereHostname   = vSphereLabel + "hostname"
+	vSphereESXhost    = vSphereLabel + "esxhost"
+	vSpherePowerState = vSphereLabel + "powerstate"
+)
 
 type VSphereClient struct {
 	// The VIM/govmomi client.
@@ -40,11 +45,11 @@ type VSphereClient struct {
 
 type Discovery struct {
 	VSphereServer string
-	logger   log.Logger
-	interval   time.Duration
+	logger        log.Logger
+	interval      time.Duration
 	Password      string
-	User	string
-	port int
+	User          string
+	port          int
 	InsecureFlag  bool
 	Debug         bool
 	DebugPath     string
@@ -52,16 +57,46 @@ type Discovery struct {
 }
 
 type SDConfig struct {
-	VSphereServer	string	`yaml:"vsphereserver"`
-	Password      string	`yaml:"password"`
-	User	string			`yaml:"auth_user"`
-	InsecureFlag  bool			`yaml:"allow_insecure_ssl"`
-	Port            int                `yaml:"port"`
-	RefreshInterval model.Duration     `yaml:"refresh_interval,omitempty"`
-	Debug         bool			`yaml:"debug,omitempty"`
-	DebugPath     string		`yaml:"debug_outpath,omitempty"`
-	DebugPathRun  string	`yaml:"debug_runpath,omitempty"`
-	XXX map[string]interface{} `yaml:",inline"`
+	VSphereServer   string                 `yaml:"vsphereserver"`
+	Password        string                 `yaml:"password"`
+	User            string                 `yaml:"auth_user"`
+	InsecureFlag    bool                   `yaml:"allow_insecure_ssl"`
+	Port            int                    `yaml:"port"`
+	RefreshInterval model.Duration         `yaml:"refresh_interval,omitempty"`
+	Debug           bool                   `yaml:"debug,omitempty"`
+	DebugPath       string                 `yaml:"debug_outpath,omitempty"`
+	DebugPathRun    string                 `yaml:"debug_runpath,omitempty"`
+	XXX             map[string]interface{} `yaml:",inline"`
+}
+
+type VSphereVersion struct {
+	// The product name. Example: "VMware vCenter Server", or "VMware ESXi".
+	Product string
+
+	// The major version. Example: If "6.5.1" is the full version, the major
+	// version is "6".
+	Major int
+
+	// The minor version. Example: If "6.5.1" is the full version, the minor
+	// version is "5".
+	Minor int
+
+	// The patch version. Example: If "6.5.1" is the full version, the patch
+	// version is "1".
+	Patch int
+
+	// The build number. This is usually a lengthy integer. This number should
+	// not be used to compare versions on its own.
+	Build int
+}
+
+// tagsMinVersion is the minimum vSphere version required for tags.
+var tagsMinVersion = VSphereVersion{
+	Product: "VMware vCenter Server",
+	Major:   6,
+	Minor:   0,
+	Patch:   0,
+	Build:   2559268,
 }
 
 var (
@@ -75,12 +110,12 @@ var (
 func NewDiscovery(conf *SDConfig, logger log.Logger) *Discovery {
 	return &Discovery{
 		VSphereServer: conf.VSphereServer,
-		User: conf.User,
-		Password: conf.Password,
-		InsecureFlag: conf.InsecureFlag,
-		interval: time.Duration(conf.RefreshInterval),
-		port: conf.Port,
-		logger: logger,
+		User:          conf.User,
+		Password:      conf.Password,
+		InsecureFlag:  conf.InsecureFlag,
+		interval:      time.Duration(conf.RefreshInterval),
+		port:          conf.Port,
+		logger:        logger,
 	}
 
 }
@@ -147,6 +182,66 @@ func (d *Discovery) EnableDebug() error {
 	return nil
 }
 
+func parseVersion(info types.AboutInfo) (VSphereVersion, error) {
+	name := info.Name
+	version := info.Version
+	build := info.Build
+
+	v := VSphereVersion{
+		Product: name,
+	}
+	s := strings.Split(version, ".")
+	if len(s) > 3 {
+		return v, fmt.Errorf("version string %q has more than 3 components", version)
+	}
+	var err error
+	v.Major, err = strconv.Atoi(s[0])
+	if err != nil {
+		return v, fmt.Errorf("could not parse major version %q from version string %q", s[0], version)
+	}
+	v.Minor, err = strconv.Atoi(s[1])
+	if err != nil {
+		return v, fmt.Errorf("could not parse minor version %q from version string %q", s[1], version)
+	}
+	v.Patch, err = strconv.Atoi(s[2])
+	if err != nil {
+		return v, fmt.Errorf("could not parse patch version %q from version string %q", s[2], version)
+	}
+	v.Build, err = strconv.Atoi(build)
+	if err != nil {
+		return v, fmt.Errorf("could not parse build version string %q", build)
+	}
+
+	return v, nil
+}
+
+func (d *Discovery) isEligibleTagEndpoint() (bool, error) {
+	client, err := d.vmClient()
+	if err != nil {
+		return false, fmt.Errorf("Failed to  setup vSphere client: %s", err)
+	}
+	if client.ServiceContent.About.ApiType != "VirtualCenter" {
+		return false, fmt.Errorf("Api Type is not VirtualCenter")
+	}
+	version, err := parseVersion(client.Client.ServiceContent.About)
+	if err != nil {
+		return false, fmt.Errorf("Failed to parse vSphere version: %s", err)
+	}
+	if version.Major < tagsMinVersion.Major {
+		return false, fmt.Errorf("Current major version: %d is older than minimum required: %d", version.Major, tagsMinVersion.Major)
+	}
+	if version.Minor > tagsMinVersion.Minor {
+		return true, nil
+	}
+	if version.Patch > tagsMinVersion.Patch {
+		return true, nil
+	}
+	if version.Build < tagsMinVersion.Build {
+		return false, fmt.Errorf("Current version %d.%d.%d %d is older than required %d.%d.%d %d", version.Major, version.Minor, version.Patch, version.Build, tagsMinVersion.Major, tagsMinVersion.Minor, tagsMinVersion.Patch, tagsMinVersion.Build)
+	}
+	return true, nil
+}
+
 // Client returns a new client for accessing VMWare vSphere Tags.
 func (d *Discovery) Client() (*VSphereClient, error) {
 
@@ -172,17 +267,10 @@ func (d *Discovery) Client() (*VSphereClient, error) {
 		return nil, fmt.Errorf("Error setting up client: %s", err)
 	}
 
-	level.Info(d.logger).Log("msg", "VMWare vSphere Client configured for URL", "info",d.VSphereServer)
+	level.Info(d.logger).Log("msg", "VMWare vSphere Client configured for URL", "info", d.VSphereServer)
 
-	// Skip the rest of this function if we are not setting up the tags client. This is if
-	//TODO Validate endpoint version
-	//	if !isEligibleTagEndpoint(client.vimClient) {
-	//		log.Printf("[WARN] Connected endpoint does not support tags (%s)", viapi.ParseVersionFromClient(client.vimClient))
-	//		return client, nil
-	//	}
-
-	// Otherwise, connect to the CIS REST API for tagging.
-	level.Info(d.logger).Log("msg", "Logging in toCIS REST login", "info",d.VSphereServer )
+	//Connect to the CIS REST API for tagging
+	level.Info(d.logger).Log("msg", "Logging in toCIS REST login", "info", d.VSphereServer)
 	client.tagsClient = tags.NewClient(u, d.InsecureFlag, "")
 	tctx, tcancel := context.WithTimeout(context.Background(), defaultAPITimeout)
 	defer tcancel()
@@ -198,7 +286,6 @@ func (d *Discovery) Client() (*VSphereClient, error) {
 
 // Client returns a new client for accessing VMWare vSphere.
 func (d *Discovery) vmClient() (*govmomi.Client, error) {
-
 
 	u, err := url.Parse("https://" + d.VSphereServer + "/sdk")
 	if err != nil {
@@ -274,7 +361,11 @@ func (d *Discovery) refresh() (tg *targetgroup.Group, err error) {
 	tg = &targetgroup.Group{
 		Source: d.VSphereServer,
 	}
-
+	//Do we have support for tags
+	tagsAvailable, err := d.isEligibleTagEndpoint()
+	if err != nil {
+		level.Warn(d.logger).Log("msg", "vSphere tags not supported", "err", err)
+	}
 
 	for _, vm := range vms {
 		ipaddress := vm.Summary.Guest.IpAddress
@@ -285,29 +376,36 @@ func (d *Discovery) refresh() (tg *targetgroup.Group, err error) {
 
 		address := net.JoinHostPort(ipaddress, fmt.Sprintf("%d", d.port))
 
-		tagIDs, err := tagsclient.ListAttachedTags(ctx, vm.ManagedEntity.Reference().Value,"VirtualMachine" )
-		if err != nil {
-			level.Error(d.logger).Log("msg", "Failed to get attached tags", "err", err, "node", vm.Summary.Config.Name)
-			continue
-		}
-
 		labels := model.LabelSet{
-			vSphereLabel + "hostname": model.LabelValue(vm.Summary.Config.Name),
+			model.LabelName(vSphereHostname):    model.LabelValue(vm.Summary.Config.Name),
 			model.LabelName(model.AddressLabel): model.LabelValue(address),
+			model.LabelName(vSpherePowerState):  model.LabelValue(vm.Summary.Runtime.PowerState),
+		}
+		if vm.Summary.Runtime.Host.Value != "" {
+			labels[model.LabelName(vSphereESXhost)] = model.LabelValue(vm.Summary.Runtime.Host.Value)
 		}
 
-		for _, item := range tagIDs {
-			vmtag, err := tagsclient.GetTag(ctx, item)
+		if tagsAvailable {
+
+			tagIDs, err := tagsclient.ListAttachedTags(ctx, vm.ManagedEntity.Reference().Value, "VirtualMachine")
 			if err != nil {
-				level.Error(d.logger).Log("msg", "Failed to get vm tag", "err", err, "node", vm.Summary.Config.Name)
+				level.Error(d.logger).Log("msg", "Failed to get attached tags", "err", err, "node", vm.Summary.Config.Name)
 				continue
 			}
-			tagName := strutil.SanitizeLabelName(vmtag.Name)
-			if strings.HasPrefix(tagName, "__meta_") {
-				labels[model.LabelName(tagName)] = model.LabelValue(vmtag.Description)
+
+			for _, item := range tagIDs {
+				vmtag, err := tagsclient.GetTag(ctx, item)
+				if err != nil {
+					level.Error(d.logger).Log("msg", "Failed to get vm tag", "err", err, "node", vm.Summary.Config.Name)
+					continue
+				}
+				tagName := strutil.SanitizeLabelName(vmtag.Name)
+				if strings.HasPrefix(tagName, "__meta_") {
+					labels[model.LabelName(tagName)] = model.LabelValue(vmtag.Description)
+
+				}
 
 			}
-
 		}
 
 		tg.Targets = append(tg.Targets, labels)
